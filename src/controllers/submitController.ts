73a4b2bd368feeda81f2df79551407ee7ab15563
Bohdan Stupak <bstupak@acceptic.com>
2019-01-02 16:43:22 +0200

    Refactor submit controller to contain it's specific markup

--- src/controllers/submitController.ts
+++ src/controllers/submitController.ts
@@ -9,38 +9,95 @@ export class SubmitController implements IController {
 
     private routeToSubmit : Route;
 
-    go(): void {
-        this.mymap.addEventListener('mousemove', (e : L.LeafletEvent) => {
-            const mouseEvent = e as L.LeafletMouseEvent
+    private controllerTemplate : string =
+    `<div id="submit-modal" class="submit-modal">
+        <div class="submit-modal-content">
+        <span class="submit-modal"></span>
+        <div>
+            <label for="route-name">Name</label>
+            <input type="text" id="route-name" placeholder="Enter route name...">
+        </div>
+        <button id="route-submit-button">Submit</button>
+        </div>
+    </div>`
+
+    private addFormSubmittedEventListener() {
+        let submitButton = document.getElementById('route-submit-button');
+        if (submitButton != null)
+            submitButton.addEventListener('click', this.submit);
+    }
+
+    private showSubmitModal = (e: CustomEvent) => {
+        let modal = document.getElementById("submit-modal");
+        if (modal) {
+            modal.style.display = 'block';
+            this.routeToSubmit = e.detail;
+        }
+    }
+
+    private addDrawingSubmittedEventListener() {
+        document.addEventListener('drawingSubmitted', this.showSubmitModal);
+    }
+
+    private addMapEventListeners() {
+        this.mymap.addEventListener('mousemove', (e: L.LeafletEvent) => {
+            const mouseEvent = e as L.LeafletMouseEvent;
             const routeDrawer = RouteDrawer.drawer;
             routeDrawer.addHypotheticalPoint(this.mymap, mouseEvent.latlng);
         });
-
-        this.mymap.addEventListener('click', (e : L.LeafletEvent) => {
-            const mouseEvent = e as L.LeafletMouseEvent
+        this.mymap.addEventListener('click', (e: L.LeafletEvent) => {
+            const mouseEvent = e as L.LeafletMouseEvent;
             const routeDrawer = RouteDrawer.drawer;
             routeDrawer.addPoint(this.mymap, mouseEvent.latlng);
         });
-
-        document.addEventListener('drawingSubmitted', (e: CustomEvent) => {
-            let modal = document.getElementById("submitmodal");
-            if (modal) {
-                modal.style.display = 'block';
-                this.routeToSubmit = e.detail;
-            }
-        });
-
-        let submitButton = document.getElementById('routeSubmitButton');
-        if (submitButton != null)
-        submitButton.addEventListener('click', this.submit);
     }
 
-    clear(): void {
+    go(): void {
+        this.addControllerTemplate();
+        this.addMapEventListeners();
+        this.addDrawingSubmittedEventListener();
+        this.addFormSubmittedEventListener();
+    }
+    private addControllerTemplate() {
+        let controllerTemplateContainer = document.getElementById('controller-template-container');
+        if (controllerTemplateContainer == null) {
+            throw new Error('Invalid html. Page should contain element with id controller-template-container');
+        }
+        let container = controllerTemplateContainer as HTMLElement;
+        container.insertAdjacentHTML('beforebegin', this.controllerTemplate);
+    }
+
+    private removeControllerTemplate() {
+        let controllerTemplateContainer = document.getElementById('submit-modal');
+        if (controllerTemplateContainer != null) {
+            var container = controllerTemplateContainer as HTMLElement;
+            container.remove();
+        }
+    }
+
+    private removeSubmitEventListeners() {
+        let submitButton = document.getElementById('route-submit-button');
+        if (submitButton != null)
+            submitButton.removeEventListener('click', this.submit);
+    }
+
+    private removeMapEventListeners() {
         this.mymap.clearAllEventListeners();
     }
 
-    submit = () : void => {
-        let nameInput = document.getElementById('routeName') as HTMLInputElement;
+    private removeFormSubmittedEventListeners() {
+        document.removeEventListener('click', this.showSubmitModal);
+    }
+
+    clear(): void {
+        this.removeMapEventListeners();
+        this.removeSubmitEventListeners();
+        this.removeFormSubmittedEventListeners();
+        this.removeControllerTemplate();
+    }
+
+    private submit = () : void => {
+        let nameInput = document.getElementById('route-name') as HTMLInputElement;
         let inputText = nameInput && nameInput.value;
         if (!inputText) {
             alert('enter route name');
